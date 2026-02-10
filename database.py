6c27ccebfcfbae98daebd447c06c15a99046c726
L@lit  Singh Sood <speedysinghs2581@gmail.com>
2026-02-10 16:59:10 +0530

    Update database.py

--- database.py
+++ database.py
@@ -4,10 +4,10 @@ import snowflake.connector
 
 def get_connection():
     return snowflake.connector.connect(
-        user="LALIT23",
-        password="Lalit12345678*",
-        account="JMCVEEC-YO11161",
-        warehouse="MY_WH",
-        database="COMPANY_DB",
-        schema="HR_SCHEMA"
+        user="*",
+        password="*",
+        account="*",
+        warehouse="*",
+        database="*",
+        schema="*"
     )
